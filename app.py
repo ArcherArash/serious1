9b7dc102332d0b3d4fa126e1714bdce54d483437
ArcherArash <40277602+ArcherArash@users.noreply.github.com>
2022-10-16 12:53:52 +0330

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 
-socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app, manage_session=False, logger=True, engineio_logger=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
